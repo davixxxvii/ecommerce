aba0c4fb92b81d566af0f0e0ea71890cbebc5396
davixxxvii@gmail.com <davixxxvii@gmail.com>
2024-04-15 17:09:48 -0300

    modificando o autoload hcodebr classes

---
+++ views-cache/header.7de693477577934f8c36ccb729347d3d.rtpl.php
@@ -0,0 +1,13 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
+<!--
+    Hcode Store by hcode.com.br
+-->
+<html>
+  <head>
+    <meta charset="utf-8">
+        <title>Hcode Store</title>  
+ 
+  </head>
+
+  <body>
+        
